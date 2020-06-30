2ec780e9ff18d7b431858d8c4b7e6ffd10f8bea6
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-06-30 11:22:16 -0700

    Added second Forecast API that calls on 5 days of cd

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $.ajax({
     method: "GET"
 }).then(function(response) {
     var weatherInfo = response;
-    console.log(weatherInfo)
+    // console.log(weatherInfo)
 
     var mainTempera = $("#mainTemp").text(response.main.temp);
     var mainCities = $("#mainCity").text(response.name);
@@ -27,11 +27,49 @@ $("#searchStart").on("click", function(event){
     var cityInput = $("#searchInput").val().trim();
 
     displayWeatherInfo(cityInput);
+    displayForecastInfo(cityInput);
 
 
 
 });
 
+
+function displayForecastInfo(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=5&appid=a849ce81cd857db4bbacc8466ea673d4&units=Imperial";
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    var forecastInfo = response;
+    console.log(response);
+    var forecastDayOne = $("#dayOneTemp").text(response.list[0].main.temp);
+    var forecastDayTwo = $("#dayTwoTemp").text(response.list[1].main.temp);
+    var forecastDayThree = $("#dayThreeTemp").text(response.list[2].main.temp);
+    var forecastDayFour = $("#dayFourTemp").text(response.list[3].main.temp);
+    var forecastDayFive = $("#dayFiveTemp").text(response.list[4].main.temp);
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
  
 
 
